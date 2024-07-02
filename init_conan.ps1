623e823488fd7d2e46b5434af1aa66a4e8419da0
Jan Juračka <jan.juracka@tescan.com>
2024-07-02 11:16:05 +0200

    fix typo in build script

--- init_conan.ps1
+++ init_conan.ps1
@@ -1,31 +1,31 @@
-if (-not (Test-Path .env)){
+if (!(Test-Path .env)){
     Write-Host Installing python environment
     py -m venv --upgrade-deps .env
 }
 
 Write-Host Installing conan
 .env\scripts\python -m pip install --require-virtualenv conan
-if (!($LastExitCode == 0))
+if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
 Write-Host Setting up conan profile
 .env\scripts\conan profile detect --force --name graph_2d_viewer_profile
-if (!($LastExitCode == 0))
+if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
 Write-Host Using conan to install deps
 .env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Debug --settings compiler.cppstd=23
-if (!($LastExitCode == 0))
+if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
 .env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Release --settings compiler.cppstd=23
-if (!($LastExitCode == 0))
+if (!($LastExitCode -eq 0))
 {
     exit   
 }
