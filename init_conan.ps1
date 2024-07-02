e0ed16f4000906dea71a290c6346c7b03aec78a5
Jan Juračka <jan.juracka@tescan.com>
2024-07-02 11:16:46 +0200

    switching standard to C++20

--- init_conan.ps1
+++ init_conan.ps1
@@ -18,13 +18,13 @@ if (!($LastExitCode -eq 0))
 }
 
 Write-Host Using conan to install deps
-.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Debug --settings compiler.cppstd=23
+.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Debug --settings compiler.cppstd=20
 if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
-.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Release --settings compiler.cppstd=23
+.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Release --settings compiler.cppstd=20
 if (!($LastExitCode -eq 0))
 {
     exit   
