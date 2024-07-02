3ec413a1b39f48b1499f4c94c71614709fbbbcc3
Jan Juračka <jan.juracka@tescan.com>
2024-07-02 13:01:47 +0200

    fix compilation of deps by using system python

--- init_conan.ps1
+++ init_conan.ps1
@@ -1,30 +1,25 @@
-if (!(Test-Path .env)){
-    Write-Host Installing python environment
-    py -m venv --upgrade-deps .env
-}
-
 Write-Host Installing conan
-.env\scripts\python -m pip install --require-virtualenv conan
+python -m pip install --require-virtualenv conan setuptools
 if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
 Write-Host Setting up conan profile
-.env\scripts\conan profile detect --force --name graph_2d_viewer_profile
+conan profile detect --force --name graph_2d_viewer_profile
 if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
 Write-Host Using conan to install deps
-.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Debug --settings compiler.cppstd=20
+conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Debug --settings compiler.cppstd=20
 if (!($LastExitCode -eq 0))
 {
     exit   
 }
 
-.env\scripts\conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Release --settings compiler.cppstd=20
+conan install . --output-folder=build_conan --profile:build graph_2d_viewer_profile --profile:host graph_2d_viewer_profile --build=missing --settings build_type=Release --settings compiler.cppstd=20
 if (!($LastExitCode -eq 0))
 {
     exit   
